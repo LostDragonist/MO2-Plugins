74f88c1ea2b9e756765ec5fddc7eba836a75103d
LostDragonist <lost.dragonist@gmail.com>
2020-01-27 13:36:59 -0700

    Add templates for IPlugin and IPluginTool

---
+++ src/templates/IPlugin.py
@@ -0,0 +1,66 @@
+from PyQt5.QtCore import QCoreApplication
+
+class PluginTemplate(mobase.IPlugin):
+    def __init__(self):
+        super().__init__()
+        self._organizer = None
+        self._parent = None
+
+    def __tr(self, str_):
+        return QCoreApplication.translate(self.name(), str_)
+
+    def init(self, organizer):
+        '''
+        Initializes the plugin and provides the main reference to the application
+        '''
+        self._organizer = organizer
+        return True
+
+    def name(self):
+        '''
+        Name of the plugin for mostly internal uses.  
+        Will be displayed in the plugin settings tab.
+        Do not translate.
+        '''
+        return "PluginTemplate"
+
+    def author(self):
+        '''
+        Author as seen in the plugin settings tab.  
+        Do not translate.
+        '''
+        return "Author"
+
+    def description(self):
+        '''
+        Description as seen in the plugin settings tab.
+        May be translated.
+        '''
+        return self.__tr("Description")
+
+    def version(self):
+        '''
+        Version as seen in the plugin settings tab.
+        '''
+        return mobase.VersionInfo(0, 0, 0, 0)
+
+    def isActive(self):
+        '''
+        Return true if plugin should be active for a given game and configuration, 
+        and false otherwise.  Inactive plugins will not be displayed in menus and their
+        notifications will be ignored.  They will still appear in the plugin settings
+        tab.
+        '''
+        return self._organizer.pluginSetting(self.name(), "enabled")
+
+    def settings(self):
+        '''
+        List of user-accessible settings for the plugin.
+        May return empty list.
+        '''
+        return [                # name      description                      default
+            mobase.PluginSetting("enabled", self.__tr("Enables the plugin"), True),
+            ]
+    
+def createPlugin():
+    return PluginTemplate()
